3e3d786d4831e713879b761e53ccdb08f944ad0a
daryanthedev <daryanarpak05@gmail.com>
2026-02-02 20:11:48 -0800

    small change to see if i can host on render

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // ─── MYSQL CONNECTION ────────────────────────────────────────
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
